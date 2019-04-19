a0583cc2926684c02ec42bfb3223347a06da1286
Lekkyjay <lekkyjayjay@yahoo.com>
2019-04-19 15:48:20 +0200

    Added browserSync and watch

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,23 @@ function style() {
     //1, where are my scss files
     return gulp.src('./scss/**/*.scss')
     //2, pass those files throught sass compiler
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
     //3, where do I save the compiled css
         .pipe(gulp.dest('./css'))
+    //4, stream changes to all browser
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
